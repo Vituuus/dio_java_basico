65460bdc63492354c1f58526dd437c5a1bd7bfc0
Vituuus <demouravitor2047@gmail.com>
2022-10-14 17:53:04 -0300

    Criação dos códigos de operadores  e comentários explicando cada um

---
+++ projetoUmJava.java/src/edu/vitor/segundasemana/OperadoresLogicos.java
@@ -0,0 +1,20 @@
+package edu.vitor.segundasemana;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+       boolean a,b;
+       
+       a = true;
+       b = true;
+
+       if (a && b) // Para A e B necessáriamentes iguais o resultado é exibido (Tanto positivo quanto negativo)
+            System.out.println("A e B são iguas");
+
+       a = true;
+       b = false;
+
+       if (a || b)// Para um dos dois positivos o resultado é exibido
+            System.out.println("Uma das variáveis é positiva");
+    }
+      
+}
---
+++ projetoUmJava.java/src/edu/vitor/segundasemana/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package edu.vitor.segundasemana;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int a = 5; // Declaração e atribuição de um valor para a
+        int b = 10;// Declaração e atribuição de um valor para b
+
+        boolean simNao = (a > b);// > é utilizado para verificar se A é Maior que B
+            System.out.println("A é 'MAIOR' que B : "+ simNao);
+        simNao = (a < b);// < é utilizado para verificar se A é Menor que B
+            System.out.println("A é 'MENOR' que B : " + simNao);
+        simNao = (a == b);// == é utilizado para verificar se A é Igual que B (Note que == é diferente de =, = é utilizado para atribuir um valor ex: a = 10, neste caso a variável a vale 10, já o == serve para comparar valores, ex: a==b, neste caso o que está acontecendo é uma comparação de a e b)  
+            System.out.println("A é 'IGUAL' a B : " + simNao);
+        simNao = (a != b);// != é utilizado para verificar se A é Diferente que B       
+            System.out.println("A é 'DIFERENTE' de B : " + simNao);
+    }
+}
